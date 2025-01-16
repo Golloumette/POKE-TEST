c170ea4674373c8b6f1ea7759018c3fef5e9bea9
carol <caroline.boucau59@gmail.com>
2025-01-16 10:49:33 +0100

    test unit

--- tests/test_unitaire.py
+++ tests/test_unitaire.py
@@ -30,6 +30,16 @@ def test_fetch_random_pokemons():
     assert response.status_code == 200
     assert len(response.json()) == 1
 
+def test_create_trainer():
+    """
+    Test pour créer un treaner
+    """
+    response = client.post("trainers/", json={
+       "name" : "sacha",
+       "birthdate" : "1986-05-21" 
+    })
+    assert response.status_code == 200
+
 
 def test_battle_endpoint_mock_first_wins():
     """
@@ -53,3 +63,19 @@ def test_battle_endpoint_mock_draw():
     })
     assert response.status_code == 200
     assert response.json()["winner"] == "draw"
+
+def test_getTrainer():
+    response = client.get("trainers/1")
+    assert response.status_code == 200
+    trainer = response.json()
+    assert "name" in trainer 
+    assert trainer["name"] == "sacha"
+
+def test_return_mock(mocker):
+    mocker.patch(
+      "main.get_double",
+      return_value=10
+   )
+    response = client.get("/5")
+    assert response.json() == 10
+    assert response.status_code == 200
